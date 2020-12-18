e772073aff018bb411876f83bcb39f2124b2f13c
Junyong Suh <jsuh@linecorp.com>
2020-12-19 08:49:04 +0900

    feat: redis added

---
+++ libs/redis.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import os
+import redis as redis_heroku
+
+r = redis_heroku.from_url(os.environ.get("REDIS_URL"), charset="utf-8", decode_responses=True)
+
+
+def mark(channel_id, user_id):
+    r.set(generate_key(channel_id, user_id), 1)
+
+
+def status(channel_id, user_id):
+    return r.get(generate_key(channel_id, user_id))
+
+
+def generate_key(channel_id, user_id):
+    now = datetime.now()
+    return f"{channel_id}:{now.year}:{now.month:02d}:{user_id}"
+
