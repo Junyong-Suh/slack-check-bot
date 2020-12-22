f5097b7a5e77ddb5e7fe7bdbca8944419bbb0ea6
Junyong Suh <jsuh@linecorp.com>
2020-12-22 18:31:38 +0900

    fix: try-catch for requests

--- libs/slack.py
+++ libs/slack.py
@@ -5,13 +5,23 @@ from libs import logger
 
 # send a message to Slack
 def send_message(message):
-    response = requests.post(
-        url=c.SLACK_API_URL,
-        json=message,
-        headers={
-            "Authorization": c.SLACK_APP_TOKEN,
-            "Content-type": "application/json; charset=utf-8"  # missing_charset if omitted
-        }
-    )
-    logger.info(response.json())
-    return response
+    try:
+        response = requests.post(
+            url=c.SLACK_API_URL,
+            json=message,
+            headers={
+                "Authorization": c.SLACK_APP_TOKEN,
+                "Content-type": "application/json; charset=utf-8"  # missing_charset if omitted
+            }
+        )
+        response.raise_for_status()
+        logger.info(response.json())
+        return response
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Error: ", err)
