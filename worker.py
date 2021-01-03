cd4e584e98383647490d6341ba6b763b50f10aa6
Junyong Suh <jsuh@linecorp.com>
2021-01-03 19:55:47 +0900

    fix: setup credentials for workers

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 import os
 import redis as redis_heroku
 from rq import Worker, Queue, Connection
+from libs import setup
 
 listen = ['high', 'default', 'low']
 redis_url = os.environ.get("REDIS_URL", 'redis://localhost:6379')
@@ -8,5 +9,6 @@ conn = redis_heroku.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
+        setup.setup_credentials()
         worker = Worker(map(Queue, listen))
         worker.work()
